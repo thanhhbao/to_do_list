a1bf411e8cd7e87db9c9ad0251a0e0cb20bfd2d8
thanhhbao <thanhhbao4123@gmail.com>
2025-06-24 18:15:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/views/home/widget/home_view.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
---
+++ lib/utils/todo_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    this.onChanged,
+    required this.deleteFunction,
+  });
+
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 0),
+      child: Slidable(
+        endActionPane: ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+            onPressed: deleteFunction,
+            icon: Icons.delete,
+            borderRadius: BorderRadius.circular(15),
+            backgroundColor: const Color.fromARGB(255, 207, 56, 45),
+          )
+        ]),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.black, borderRadius: BorderRadius.circular(15)),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                side: BorderSide(color: Colors.white),
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 182, 180, 180),
+                    fontSize: 20,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                    decorationColor: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    decorationThickness: 2),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
